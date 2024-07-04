0334dabcbe0f5117cc3017cd8e8b304c105dede3
Haruki <aratashiharuki@gmail.com>
2024-07-05 00:17:03 +0300

    Add find method

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,13 @@ const llObject = new LinkedList<number>()
 llObject.push(1)
 llObject.push(2)
 llObject.push(3)
+llObject.push(4)
+
 
 for (let node of llObject) {
     console.log(node)
 }
+
+console.log(llObject.find(value => {
+    return value % 2 == 0
+}, 1))
--- src/linked-list/index.ts
+++ src/linked-list/index.ts
@@ -6,25 +6,7 @@ type LinkedNode<T> = {
 type LinkedPredictFunction<T> = (value: T) => boolean
 
 class LinkedList<T> {
-    private _head: LinkedNode<T> | null = null
-
-    push(value: T): void {
-        const node: LinkedNode<T> = {value: value, next: null}
-        
-        if (!this._head) {
-            this._head = node
-            return
-        }
-
-        let nodePointer = this._head
-        while (nodePointer.next) {
-            nodePointer = nodePointer.next
-        }
-
-        console.log(nodePointer.value)
-
-        nodePointer.next = node
-    }
+    private _head: LinkedNode<T> | null = null;
 
     [Symbol.iterator]() : Iterator<LinkedNode<T>> {
         let nodePointer: LinkedNode<T> | null = this._head;
@@ -47,6 +29,40 @@ class LinkedList<T> {
                 }
             }
         }
+    }
+
+    push(value: T): void {
+        const node: LinkedNode<T> = {value: value, next: null}
+        
+        if (!this._head) {
+            this._head = node
+            return
+        }
+
+        let nodePointer = this._head
+        while (nodePointer.next) {
+            nodePointer = nodePointer.next
+        }
+
+        console.log(nodePointer.value)
+
+        nodePointer.next = node
+    }
+
+    find(predictFn: LinkedPredictFunction<T>, limit?: number): LinkedNode<T>[] {
+        const suitableNodes: LinkedNode<T>[] = []
+        const iterator: Iterator<LinkedNode<T>> = this[Symbol.iterator]()
+
+        while (true) {
+            const result = iterator.next()
+            if (result.done) break;
+
+            if (limit && suitableNodes.length == limit) break;
+
+            if (predictFn((result.value).value)) suitableNodes.push(result.value);
+        }
+
+        return suitableNodes
     }
 }
 
